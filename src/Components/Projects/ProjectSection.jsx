dfb540436707f0efca5e160c94d29d18b11b1507
Robert <r.c.cable@tutamail.com>
2021-07-09 16:05:38 -0600

    updated Java Projects section

--- src/Components/Projects/ProjectSection.jsx
+++ src/Components/Projects/ProjectSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./ProjectSection.css"
+import "./ProjectSection.css";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 const frontEndProjects = [
@@ -32,21 +32,21 @@ const frontEndProjects = [
 
 const javaProjects = [
   {
-    src: "https://user-images.githubusercontent.com/77639928/119536431-523caa80-bd46-11eb-85e4-be38a919231e.png",
+    src: "https://user-images.githubusercontent.com/77639928/125139993-ce106d80-e0ce-11eb-84e4-c28ba9ad48e1.png",
     title: "CS400 Data Structure and Algorithms Final Project",
     description:
-      "This program: -Takes command line arguments. -Creates Person objects. -Inputs them into a programmer defined Queue. -Sorts them using a programmer defined recursive Quick Sort algorithm. -Uses Comparators to sort them in more than one way.",
+      "This program takes command line arguments, creates Person objects, inputs them into a programmer defined queue, sorts them using a programmer defined recursive Quick Sort algorithm,  and uses Comparators to sort them in more than one way.",
     sourceCodeHref: "https://github.com/R-C-Cable/CS400-Final-Project",
   },
   {
-    src: "https://user-images.githubusercontent.com/77639928/124301903-b3158a80-db1d-11eb-8a42-0815a3c2d5dd.png",
+    src: "https://user-images.githubusercontent.com/77639928/125139589-06637c00-e0ce-11eb-869d-a7ca20e4bcdc.png",
     title: "CSC450 Programming III Final Project",
     description:
       "This is a simple concurrent program in Java. It simply passes a counter method with a starting and ending integer to the defined thread, MyThread. Then MyThread runs the counter method that validates the parameters, counts from the starting to ending integer, and then it repeats this in the opposite direction.",
     sourceCodeHref: "https://github.com/R-C-Cable/CSC450_Final_Project",
   },
   {
-    src: "https://user-images.githubusercontent.com/77639928/124299115-4a78de80-db1a-11eb-955b-308a80057673.png",
+    src: "https://user-images.githubusercontent.com/77639928/125139812-7540d500-e0ce-11eb-82f5-40f16ed526d9.png",
     title: "CSC375 Programing II Final Project",
     description:
       "This program asks the user for input, validates the input, and then instantiates student objects with the input. It stores them in a LinkedList, and then uses a programer defined Comparator to sort the student objects by name. Finally, it uses the programer defined toString method to output the objects to a file in a predictable manner.",
@@ -54,43 +54,53 @@ const javaProjects = [
   },
 ];
 
-
 function ProjectCard(props) {
   return (
-    <Col sm >
-    <Card id="ProjectCard"  style={{ width: "95%" , margin: "auto" }}>
-      <Card.Img variant="top" src={props.src} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.description}</Card.Text>
-      </Card.Body>
-      <Card.Footer className="text-center">
-        {props.deployHref && <Button className="m-1" target="_blank" variant="primary" href={props.deployHref}>
-          Deployed
-        </Button>}
-        <Button className="m-1" target="_blank" variant="primary" href={props.sourceCodeHref}>
-          Source Code
-        </Button>
-      </Card.Footer>
-    </Card>
+    <Col sm>
+      <Card id="ProjectCard" style={{ width: "95%" }}>
+        <Card.Img id="ProjectCardImg" variant="top" src={props.src} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>{props.description}</Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-center">
+          {props.deployHref && (
+            <Button
+              className="m-1"
+              target="_blank"
+              variant="primary"
+              href={props.deployHref}
+            >
+              Deployed
+            </Button>
+          )}
+          <Button
+            className="m-1"
+            target="_blank"
+            variant="primary"
+            href={props.sourceCodeHref}
+          >
+            Source Code
+          </Button>
+        </Card.Footer>
+      </Card>
     </Col>
-    
   );
 }
 
 function ProjectSection() {
   return (
     <Container id="ProjectSection">
-      <h1 className="mb-4">Web Development Project</h1>
+      <h1 className="mb-4">Web Development Projects</h1>
       <Row>
         {frontEndProjects.map((projects) => {
-          return <ProjectCard {...projects}/>;
+          return <ProjectCard {...projects} />;
         })}
       </Row>
-       <h1 className="m-4">Java Projects</h1>
+      <h1 className="m-4">Java Projects</h1>
       <Row>
         {javaProjects.map((projects) => {
-          return <ProjectCard {...projects}/>;
+          return <ProjectCard {...projects} />;
         })}
       </Row>
     </Container>
